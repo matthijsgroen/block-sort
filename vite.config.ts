083bf12d0e6a0d0bc5aebab143378c4a03285ece
Matthijs Groen <matthijs.groen@gmail.com>
2024-09-13 09:58:43 +0200

    switch to prompt mode

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       injectRegister: false,
 
       pwaAssets: {
