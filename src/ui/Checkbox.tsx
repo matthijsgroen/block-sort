65b6bc005a821989a01d1a31add2aa9e5895d069
Matthijs Groen <matthijs.groen@gmail.com>
2024-09-20 17:08:16 +0200

    fix duplicate styling

--- src/ui/Checkbox.tsx
+++ src/ui/Checkbox.tsx
@@ -16,7 +16,7 @@ export const Checkbox: React.FC<Props> = ({ value, label, onChange }) => (
         onChange?.(e.target.checked);
       }}
     />
-    <div className="relative w-9 h-5 bg-black/20 rounded-full peer bg-black/30 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-green-900"></div>
+    <div className="relative w-9 h-5 bg-black/20 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-green-900"></div>
     <span className="ms-3 text-sm font-medium text-gray-900">{label}</span>
   </label>
 );
