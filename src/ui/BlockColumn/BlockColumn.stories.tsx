40fd1a38b3fa5370aa998425748d3a69adc82026
Matthijs Groen <matthijs.groen@gmail.com>
2024-09-24 14:18:51 +0200

    Add more column stories

--- src/ui/BlockColumn/BlockColumn.stories.tsx
+++ src/ui/BlockColumn/BlockColumn.stories.tsx
@@ -16,6 +16,24 @@ type BlockColumnPropsAndCustomArgs = React.ComponentProps<
   limited: boolean;
 };
 
+const columnSettings = (
+  type: Column["type"],
+  columnSize = 1,
+  locked = false,
+  limited = false,
+  amountBlocks = 0
+): Column => {
+  return {
+    type,
+    columnSize,
+    locked,
+    limitColor: limited ? "aqua" : undefined,
+    blocks: Array(Math.max(Math.min(amountBlocks, columnSize), 0))
+      .fill(0)
+      .map<Block>(() => createBlock("aqua")),
+  };
+};
+
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<BlockColumnPropsAndCustomArgs> = {
   title: "BlockSort/BlockColumn",
@@ -58,32 +76,6 @@ const meta: Meta<BlockColumnPropsAndCustomArgs> = {
     onPickUp: fn(),
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-};
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-const columnSettings = (
-  type: Column["type"],
-  columnSize = 1,
-  locked = false,
-  limited = false,
-  amountBlocks = 0
-): Column => {
-  return {
-    type,
-    columnSize,
-    locked,
-    limitColor: limited ? "aqua" : undefined,
-    blocks: Array(Math.max(Math.min(amountBlocks, columnSize), 0))
-      .fill(0)
-      .map<Block>(() => createBlock("aqua")),
-  };
-};
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const SmallPlacementColumn: Story = {
-  args: {},
   render: (args) => {
     const { type, columnSize, locked, amountBlocks, limited } = args;
     const column = columnSettings(
@@ -95,5 +87,53 @@ export const SmallPlacementColumn: Story = {
     );
 
     return <BlockColumn {...args} column={column} />;
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const SmallPlacementColumn: Story = {
+  args: {
+    columnSize: 4,
+    amountBlocks: 2,
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const SmallBufferColumn: Story = {
+  args: {
+    type: "buffer",
+    columnSize: 4,
+    amountBlocks: 2,
+  },
+};
+
+export const LockedColumn: Story = {
+  args: {
+    type: "placement",
+    columnSize: 4,
+    amountBlocks: 4,
+    locked: true,
+  },
+};
+
+export const LimitedColumn: Story = {
+  args: {
+    type: "placement",
+    columnSize: 4,
+    amountBlocks: 2,
+    limited: true,
+  },
+};
+
+export const LimitedColumnSelection: Story = {
+  args: {
+    type: "placement",
+    columnSize: 4,
+    amountBlocks: 2,
+    amountSelected: 1,
+    limited: true,
   },
 };
