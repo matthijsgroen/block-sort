b9b0730aee6b68221feca556fe30409f52f7e5ac
Matthijs Groen <matthijs.groen@gmail.com>
2024-09-26 13:09:15 +0200

    Add message to storybook

---
+++ src/ui/Message/Message.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Message } from "./Message";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Message> = {
+  title: "BlockSort/Message",
+  component: Message,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+  args: {
+    message: "Hello, World!",
+  },
+  render: (args) => {
+    return (
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Message {...args} />
+      </div>
+    );
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {},
+};
