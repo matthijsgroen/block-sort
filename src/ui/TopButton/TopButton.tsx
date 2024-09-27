5b278e98ed516e81beeb5bc1e010ae9dcacecc71
Matthijs Groen <matthijs.groen@gmail.com>
2024-09-27 17:27:42 +0200

    Refactor wooden button

--- src/ui/TopButton/TopButton.tsx
+++ src/ui/TopButton/TopButton.tsx
@@ -1,38 +1,39 @@
 import clsx from "clsx";
 
-import styles from "./TopButton.module.css";
+import { WoodButton } from "../WoodButton/WoodButton";
 
 type Props = {
   onClick?: VoidFunction;
   buttonType: "restart" | "back" | "settings" | "close";
 };
 
-export const TopButton: React.FC<Props> = ({ onClick, buttonType }) => {
-  return (
-    <button
-      onPointerDown={onClick}
-      className={clsx(
-        {
-          "text-2xl":
-            buttonType === "back" ||
-            buttonType === "settings" ||
-            buttonType === "close",
-          "text-4xl": buttonType === "restart",
-        },
-        "inline-block size-block border border-black rounded-full",
-        styles.buttonBackground
-      )}
+export const TopButton: React.FC<Props> = ({ onClick, buttonType }) => (
+  <WoodButton onClick={onClick}>
+    <span
+      className={clsx({
+        "text-2xl":
+          buttonType === "back" ||
+          buttonType === "settings" ||
+          buttonType === "close",
+        "text-4xl": buttonType === "restart",
+      })}
     >
-      {buttonType === "back" && <span className="inline-block">&larr;</span>}
+      {buttonType === "back" && (
+        <span className="inline-block translate-y-[2px]">&larr;</span>
+      )}
       {buttonType === "restart" && (
         <span className="inline-block -translate-y-[5px] -translate-x-[2px]">
           &#10226;
         </span>
       )}
-      {buttonType === "settings" && <span className="inline-block">⛭</span>}
-      {buttonType === "close" && (
-        <span className="inline-block -translate-y-[2px]">&times;</span>
+      {buttonType === "settings" && (
+        <span className="inline-block translate-y-[2px] translate-x-[-1px]">
+          ⛭
+        </span>
       )}
-    </button>
-  );
-};
+      {buttonType === "close" && (
+        <span className="inline-block translate-x-[-1px]">&times;</span>
+      )}
+    </span>
+  </WoodButton>
+);
---
+++ src/ui/WoodButton/WoodButton.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren } from "react";
+import clsx from "clsx";
+
+import styles from "./WoodButton.module.css";
+
+type Props = {
+  onClick?: VoidFunction;
+};
+export const WoodButton: React.FC<PropsWithChildren<Props>> = ({
+  onClick,
+  children,
+}) => {
+  return (
+    <button
+      onPointerDown={onClick}
+      className={clsx(
+        "inline-block border h-block border-black rounded-3xl min-w-block px-2",
+        styles.woodBackground
+      )}
+    >
+      {children}
+      <span
+        className={clsx(styles.woodTexture, "inline-block h-block rounded-3xl")}
+      ></span>
+      <span
+        className={clsx(
+          styles.woodLabel,
+          "inline-block min-w-block px-2 h-block rounded-3xl"
+        )}
+      >
+        {children}
+      </span>
+    </button>
+  );
+};
