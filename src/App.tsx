9d4ae293e45af6597c7f2bfcda161fd0e130bf93
Matthijs Groen <matthijs.groen@gmail.com>
2024-10-04 08:23:29 +0200

    Reduce asset sizes

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import { NormalMode } from "./modules/GameModi/NormalMode.tsx";
 import { ZenMode } from "./modules/GameModi/ZenMode.tsx";
 import { BackgroundProvider } from "./modules/Layout/BackgroundContext.tsx";
 import { Settings } from "./modules/Settings/index.tsx";
-import { sound } from "./audio.ts";
-import PWABadge from "./PWABadge.tsx";
-import { getActiveTheme, ThemeProvider } from "./support/ThemeProvider.tsx";
-import { THEMES } from "./featureFlags.ts";
 import { getThemeSong } from "./support/themeMusic.tsx";
+import { getActiveTheme, ThemeProvider } from "./support/ThemeProvider.tsx";
+import { sound, Stream } from "./audio.ts";
+import { THEMES } from "./featureFlags.ts";
+import PWABadge from "./PWABadge.tsx";
 
 export const App: React.FC = () => {
   const [settingsOpen, setSettingsOpen] = useState(false);
@@ -27,19 +27,19 @@ export const App: React.FC = () => {
   const song = getThemeSong(theme);
 
   useEffect(() => {
-    sound.stopAllInStream("music");
+    sound.stopAllInStream(Stream.music);
     if (musicEnabled) {
       sound.play(song);
     }
   }, [song]);
 
   useEffect(() => {
-    sound.setStreamEnabled("effects", soundEnabled);
-    sound.setStreamEnabled("music", musicEnabled);
+    sound.setStreamEnabled(Stream.effects, soundEnabled);
+    sound.setStreamEnabled(Stream.music, musicEnabled);
     if (musicEnabled) {
       sound.play(song);
     } else {
-      sound.stopAllInStream("music");
+      sound.stopAllInStream(Stream.music);
     }
   }, [soundEnabled, musicEnabled]);
 
@@ -62,16 +62,16 @@ export const App: React.FC = () => {
             musicEnabled={musicEnabled}
             themesEnabled={themesEnabled}
             onSoundChange={(effectsEnabled) => {
-              sound.setStreamEnabled("effects", effectsEnabled);
+              sound.setStreamEnabled(Stream.effects, effectsEnabled);
               setSoundEnabled(effectsEnabled);
             }}
             onMusicChange={(musicEnabled) => {
-              sound.setStreamEnabled("music", musicEnabled);
+              sound.setStreamEnabled(Stream.music, musicEnabled);
               if (musicEnabled) {
                 // tie playback to user interaction
                 sound.play(song);
               } else {
-                sound.stopAllInStream("music");
+                sound.stopAllInStream(Stream.music);
               }
               setMusicEnabled(musicEnabled);
             }}
