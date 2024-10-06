2b8b253687900c4cdca6e4ff4d6eb14db557dc57
Matthijs Groen <matthijs.groen@gmail.com>
2024-10-06 07:59:46 +0200

    Update ramp-up of difficulty

--- src/game/level-settings/normalSettings.ts
+++ src/game/level-settings/normalSettings.ts
@@ -2,7 +2,7 @@ import { LevelSettings } from "../level-creation/generateRandomLevel";
 
 export const getNormalSettings = (difficulty: number): LevelSettings => ({
   amountColors: Math.min(1 + difficulty, 10),
-  stackSize: Math.min(Math.max(difficulty - 3, 4), 7),
+  stackSize: Math.min(Math.max(2 + Math.floor(difficulty / 2), 4), 7),
   extraPlacementStacks: difficulty < 2 ? 1 : 2,
   extraPlacementLimits: difficulty > 9 ? 1 : undefined,
   hideBlockTypes: "none",
