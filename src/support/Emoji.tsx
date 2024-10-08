7fb6885032e3cb945a51bdc58afc04c7ac4cde50
Matthijs Groen <matthijs.groen@gmail.com>
2024-10-08 19:55:38 +0200

    Fix emoji as text

--- src/support/Emoji.tsx
+++ src/support/Emoji.tsx
@@ -4,5 +4,7 @@ export const TextEmoji: React.FC<{ emoji: string; className?: string }> = ({
   emoji,
   className,
 }) => {
-  return <span className={clsx("font-mono", className)}>{emoji}&#xFE0E;</span>;
+  return (
+    <span className={clsx("font-mono", className)}>{`${emoji}\uFE0E`}</span>
+  );
 };
