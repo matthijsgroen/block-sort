109715d78213d397ad3792d7fd3ce9b0ff0bd276
Matthijs Groen <matthijs.groen@gmail.com>
2024-11-24 20:35:09 +0100

    Update spring level seeds

--- src/game/level-types/spring.spec.ts
+++ src/game/level-types/spring.spec.ts
@@ -2,7 +2,11 @@ import { describe } from "vitest";
 
 import { testDifficulties } from "../level-settings/difficultyTester";
 
-import { getSpringSettings } from "./spring";
+import { getEasySpringSettings, getSpringSettings } from "./spring";
+
+describe(getEasySpringSettings, () => {
+  testDifficulties(getEasySpringSettings);
+});
 
 describe(getSpringSettings, () => {
   testDifficulties(getSpringSettings);
