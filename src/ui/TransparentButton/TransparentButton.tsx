687266b028a5605d6269c18f236f8a29cb3c3932
Matthijs Groen <matthijs.groen@gmail.com>
2024-11-24 20:38:16 +0100

    Add select none to button text

--- src/ui/TransparentButton/TransparentButton.tsx
+++ src/ui/TransparentButton/TransparentButton.tsx
@@ -16,7 +16,7 @@ export const TransparentButton: React.FC<Props> = ({
   size = "default"
 }) => {
   const classNames = clsx(
-    "inline-block rounded-full border border-black p-2 shadow-md",
+    "inline-block select-none rounded-full border border-black p-2 shadow-md",
     "text-center align-middle text-black",
     "transition-transform active:scale-90",
     {
