6710ecb151d94588fec43651928d5aa1969c0b0c
Matthijs Groen <matthijs.groen@gmail.com>
2024-11-27 21:25:30 +0100

    Add descriptions

--- src/ui/Checkbox.tsx
+++ src/ui/Checkbox.tsx
@@ -24,8 +24,12 @@ export const Checkbox: React.FC<Props> = ({
     />
     <div className="flex-0 peer relative h-5 w-9 rounded-full bg-black/20 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-900 peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full"></div>
     <div className="flex-1">
-      <p className="ms-3 text-sm font-medium text-gray-900">{label}</p>
-      {description && <p className="ms-3 text-xs italic">{description}</p>}
+      <p className="ms-3 select-none text-sm font-medium text-gray-900">
+        {label}
+      </p>
+      {description && (
+        <p className="ms-3 select-none text-xs italic">{description}</p>
+      )}
     </div>
   </label>
 );
