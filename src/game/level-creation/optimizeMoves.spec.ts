b0de42ba3a189be69c46cca7cae63895f73fc2d0
Matthijs Groen <matthijs.groen@gmail.com>
2024-12-04 14:30:46 +0100

    update optimize moves with new seeds

--- src/game/level-creation/optimizeMoves.spec.ts
+++ src/game/level-creation/optimizeMoves.spec.ts
@@ -1,19 +1,28 @@
 import { describe, expect, it } from "vitest";
 
+import { levelSeeds } from "@/data/levelSeeds";
+import { settingsHash } from "@/support/hash";
 import { mulberry32 } from "@/support/random";
 
 import { moveBlocks } from "../actions";
 import { getNormal5Settings, getNormalSettings } from "../level-types/normal";
 import { hasWon } from "../state";
+import { LevelSettings } from "../types";
 
 import { optimizeMoves } from "./optimizeMoves";
 import { generatePlayableLevel } from "./tactics";
 
+const getSeed = (settings: LevelSettings, offset = 0) => {
+  const hash = settingsHash(settings);
+  return levelSeeds[hash][offset][0];
+};
+
 describe(optimizeMoves, () => {
   it("returns the same level", async () => {
-    const seed = 517311761;
+    const settings = getNormalSettings(1);
+    const seed = getSeed(settings);
     const random = mulberry32(seed);
-    const level = await generatePlayableLevel(getNormalSettings(1), { random });
+    const level = await generatePlayableLevel(settings, { random });
     const result = optimizeMoves(level);
     expect(result.columns).toBe(level.columns);
     expect(result.colors).toBe(level.colors);
@@ -21,21 +30,21 @@ describe(optimizeMoves, () => {
 
   it("reduces the amount of moves", async () => {
     const settings = getNormal5Settings(11);
+    const seed = getSeed(settings, 4);
 
-    const seed = 517311761;
     const random = mulberry32(seed);
 
     const level = await generatePlayableLevel(settings, { random, seed });
     const result = optimizeMoves(level);
-    expect(level.moves.length).toBeGreaterThan(350);
+    expect(level.moves.length).toBeGreaterThan(250);
     expect(result.moves.length).toBeLessThan(level.moves.length);
-    expect(result.moves.length).toBeLessThan(150);
+    expect(result.moves.length).toBeLessThan(110);
   }, 30_000);
 
   it("can still complete the level", async () => {
     const settings = getNormal5Settings(11);
+    const seed = getSeed(settings, 4);
 
-    const seed = 517311761;
     const random = mulberry32(seed);
 
     const level = await generatePlayableLevel(settings, { random, seed });
