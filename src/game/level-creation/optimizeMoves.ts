158c0e3b12b65e01f014335c7ad2a31b61d7af6c
Matthijs Groen <matthijs.groen@gmail.com>
2025-03-19 23:29:49 +0100

    Optimize some moves

--- src/game/level-creation/optimizeMoves.ts
+++ src/game/level-creation/optimizeMoves.ts
@@ -1,20 +1,38 @@
 import { findLastIndex } from "@/support/findLastIndex";
 
-import { moveBlocks } from "../actions";
+import { moveBlocks, selectFromColumn } from "../actions";
 import type { LevelState } from "../types";
 
 export const optimizeMoves = (level: LevelState): LevelState => {
   let levelClone: LevelState = JSON.parse(JSON.stringify(level)) as LevelState;
 
-  const movesWithHash: { from: number; to: number; hash: string }[] =
-    level.moves.map((move) => {
-      levelClone = moveBlocks(levelClone, move);
-      return {
-        from: move.from,
-        to: move.to,
-        hash: JSON.stringify(levelClone.columns)
-      };
-    });
+  const movesWithHash: {
+    from: number;
+    to: number;
+    amount: number;
+    bufferJump: boolean;
+    hash: string;
+  }[] = level.moves.map((move) => {
+    const selection = selectFromColumn(levelClone, move.from);
+
+    levelClone = moveBlocks(levelClone, move);
+    const fromColumn = levelClone.columns[move.from];
+    const toColumn = levelClone.columns[move.to];
+
+    const isBufferJump =
+      fromColumn.type === toColumn.type &&
+      fromColumn.type === "buffer" &&
+      fromColumn.columnSize === toColumn.columnSize &&
+      selection.length === fromColumn.columnSize;
+
+    return {
+      from: move.from,
+      bufferJump: isBufferJump,
+      amount: selection.length,
+      to: move.to,
+      hash: JSON.stringify(levelClone.columns)
+    };
+  });
 
   for (let i = 0; i < movesWithHash.length; i++) {
     const hash = movesWithHash[i].hash;
@@ -26,6 +44,21 @@ export const optimizeMoves = (level: LevelState): LevelState => {
       movesWithHash.splice(i + 1, highestIndex - i);
     }
   }
+  for (let i = 0; i < movesWithHash.length; i++) {
+    if (movesWithHash[i].bufferJump) {
+      // find move that moved blocks to the from column, if directly before
+      const j = i - 1;
+      if (movesWithHash[j].to === movesWithHash[i].from) {
+        if (movesWithHash[j].amount === movesWithHash[i].amount) {
+          // Target column is empty at the time, since action was directly before
+          movesWithHash[j].to = movesWithHash[i].to;
+          movesWithHash.splice(i, 1);
+          i--;
+        }
+      }
+    }
+  }
+
   const optimizedMoves = movesWithHash.map((move) => ({
     from: move.from,
     to: move.to
