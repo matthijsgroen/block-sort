dd1440e79021350f1565bb19b1e9c1ac164bee27
Matthijs Groen <matthijs.groen@gmail.com>
2025-03-21 16:05:58 +0100

    add tests for data migration

--- src/support/migrateLevelState.ts
+++ src/support/migrateLevelState.ts
@@ -2,10 +2,10 @@ import type { LevelState } from "@/game/types";
 
 import { isVersion1, migrateFromVersion1 } from "./migration/levelV1";
 
-export const migrateLevelState = (value: LevelState): LevelState => {
+export const migrateLevelState = (value: unknown): LevelState => {
   if (isVersion1(value)) {
     return migrateFromVersion1(value);
   }
 
-  return value;
+  return value as LevelState;
 };
